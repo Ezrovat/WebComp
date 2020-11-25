89996970a1b0aed65d6a3959cad241da3259bcef
Ezrovat <danic97@gmail.com>
2020-11-25 10:48:18 +0100

    first commit

---
+++ GestioneSegreteria/src/main/java/it/unical/mat/webcomp/segreteria/GestioneSegreteriaApplication.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webcomp.segreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneSegreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneSegreteriaApplication.class, args);
+	}
+
+}
---
+++ GestioneSegreteria/src/test/java/it/unical/mat/webcomp/segreteria/GestioneSegreteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webcomp.segreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneSegreteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
